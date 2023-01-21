66aab62fdf43b69b5f41b8a5850c76726de76e37
RyanRosa <ryan.rosa.profissional@gmail.com>
2023-01-21 10:39:57 -0300

    Primeira parte do parseamento do json

--- src/Principal.java
+++ src/Principal.java
@@ -8,15 +8,26 @@ public class Principal {
 
 	public static void main(String[] args) throws Exception {
 
-		HttpRequest request = HttpRequest.newBuilder().GET()
-
-				.uri(URI.create("https://imdb-api.com/en/API/Top250Movies/k_2u4vtzp4")).build();
-
+		HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create("https://imdb-api.com/en/API/Top250Movies/k_5qyczg7n")).build();
 		HttpClient httpClient = HttpClient.newBuilder().build();
-
 		HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
 
-		System.out.println(response.body());
+		String json = response.body();
+		String[] filmes = parsearJson(json);
+
+		for (String filme : filmes) {
+			System.out.println(filme);
+		}
+	}
+
+	public static String[] parsearJson(String json) {
+		String[] filmes = json.split("},");
+		for (int i = 0; i < filmes.length; i++) {
+			filmes[i] = filmes[i].replaceAll("\\{", "");
+		}
+		filmes[249] = filmes[249].replaceAll("}],\"errorMessage\":\"\"}", "");
+		filmes[0] = filmes[0].replaceAll("\"items\":\\[", "");
+		return filmes;
 	}
 
 }
